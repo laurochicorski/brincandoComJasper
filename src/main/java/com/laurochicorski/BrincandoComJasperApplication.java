1356ffa9a83564a2474dece9eba77e469efd1a71
Lauro Chicorski <laurocoelhoc@gmail.com>
2016-04-11 18:34:46 -0300

    Commit Inicial

---
+++ src/main/java/com/laurochicorski/BrincandoComJasperApplication.java
@@ -0,0 +1,12 @@
+package com.laurochicorski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrincandoComJasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrincandoComJasperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/laurochicorski/BrincandoComJasperApplicationTests.java
@@ -0,0 +1,18 @@
+package com.laurochicorski;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BrincandoComJasperApplication.class)
+@WebAppConfiguration
+public class BrincandoComJasperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
